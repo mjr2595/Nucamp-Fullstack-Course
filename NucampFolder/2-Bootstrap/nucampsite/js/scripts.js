506b5c8a003203f6f50c146470cc34b26ee8c3c5
mjr2595 <michaeljohnraymond@gmail.com>
2022-12-17 11:08:54 -0600

    Week 4 Assignment

--- NucampFolder/2-Bootstrap/nucampsite/js/scripts.js
+++ NucampFolder/2-Bootstrap/nucampsite/js/scripts.js
@@ -11,4 +11,10 @@ $(function () {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("toggle");
+    });
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("toggle");
+    });
 });
