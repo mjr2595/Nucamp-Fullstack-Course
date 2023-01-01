acdc54a8ec215de58b8fd476050a1f18e59f5f72
mjr2595 <michaeljohnraymond@gmail.com>
2023-01-01 11:28:17 -0600

    I ❤️  React

---
+++ NucampFolder/3-React/i-heart-react/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import HeartsList from "./components/HeartsList";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <HeartsList />
+        </>
+    );
+}
+
+export default App;
