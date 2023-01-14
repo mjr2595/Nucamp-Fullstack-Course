2bbdefb299fddff6c6c0a41956d2b69d75ddb493
mjr2595 <michaeljohnraymond@gmail.com>
2023-01-14 12:34:06 -0600

    Week 2 Workshop | Partner list in About Page

--- NucampFolder/3-React/nucampsite/src/App.js
+++ NucampFolder/3-React/nucampsite/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
                 <Route path="contact" element={<ContactPage />} />
                 <Route path="directory" element={<CampsitesDirectoryPage />} />
                 <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+                <Route path="about" element={<AboutPage />} />
             </Routes>
             <Footer />
         </div>
