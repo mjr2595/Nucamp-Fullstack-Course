93a34aa5639cf9d98883fccc1133be82c5cf5d29
mjr2595 <michaeljohnraymond@gmail.com>
2023-02-01 22:10:51 -0600

    async partners

--- NucampFolder/3-React/nucampsite/src/App.js
+++ NucampFolder/3-React/nucampsite/src/App.js
@@ -9,6 +9,7 @@ import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import AboutPage from "./pages/AboutPage";
 import { fetchCampsites } from "./features/campsites/campsitesSlice";
+import { fetchPartners } from "./features/partners/partnersSlice";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
 
     useEffect(() => {
         dispatch(fetchCampsites());
+        dispatch(fetchPartners());
     }, [dispatch]);
 
     return (
