21f1b90c26a0110d62083bd1dcd14ba536bb6169
mjr2595 <michaeljohnraymond@gmail.com>
2023-02-16 23:55:34 -0600

    Components Part 2

---
+++ NucampFolder/4-React-Native/nucampsite/screens/CampsiteInfoScreen.js
@@ -0,0 +1,5 @@
+import RenderCampsite from "../features/campsites/RenderCampsite";
+const CampsiteInfoScreen = (props) => {
+  return <RenderCampsite campsite={props.campsite} />;
+};
+export default CampsiteInfoScreen;
--- NucampFolder/4-React-Native/nucampsite/screens/MainComponent.js
+++ NucampFolder/4-React-Native/nucampsite/screens/MainComponent.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import { View } from "react-native-animatable";
 import { CAMPSITES } from "../shared/campsites";
 import DirectoryScreen from "./DirectoryScreen";
+import CampsiteInfoScreen from "./CampsiteInfoScreen";
 
 const Main = () => {
   const [campsites, setCampsites] = useState(CAMPSITES);
-  return <DirectoryScreen campsites={campsites} />;
+  const [selectedCampsiteId, setSelectedCampsiteId] = useState();
+
+  return (
+    <View style={{ flex: 1 }}>
+      <DirectoryScreen campsites={campsites} onPress={(campsiteId) => setSelectedCampsiteId(campsiteId)} />
+      <CampsiteInfoScreen campsite={campsites.filter((campsite) => campsite.id === selectedCampsiteId)[0]} />
+    </View>
+  );
 };
 
 export default Main;
