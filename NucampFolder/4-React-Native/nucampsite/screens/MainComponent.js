2c2d1c2635ac40bc6b85e54940fd7dff8730c081
mjr2595 <michaeljohnraymond@gmail.com>
2023-02-18 00:45:56 -0600

    Navigation Part 2

--- NucampFolder/4-React-Native/nucampsite/screens/MainComponent.js
+++ NucampFolder/4-React-Native/nucampsite/screens/MainComponent.js
@@ -2,20 +2,30 @@ import { Platform, View } from "react-native";
 import Constants from "expo-constants";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import DirectoryScreen from "./DirectoryScreen";
+import HomeScreen from "./HomeScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createStackNavigator } from "@react-navigation/stack";
+
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+  headerTintColor: "#fff",
+  headerStyle: { backgroundColor: "#5637DD" },
+};
+
+const HomeNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Home" }} />
+    </Stack.Navigator>
+  );
+};
 
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator
-      initialRouteName="Directory"
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#5637DD",
-        },
-        headerTintColor: "#fff",
-      }}
-    >
+    <Stack.Navigator initialRouteName="Directory" screenOptions={screenOptions}>
       <Stack.Screen name="Directory" component={DirectoryScreen} options={{ title: "Campsite Directory" }} />
       <Stack.Screen
         name="CampsiteInfo"
@@ -36,7 +46,10 @@ const Main = () => {
         paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
       }}
     >
-      <DirectoryNavigator />
+      <Drawer.Navigator initialRouteName="Home" drawerStyle={{ backgroundColor: "#CEC8FF" }}>
+        <Drawer.Screen name="Home" component={HomeNavigator} options={{ title: "Home" }} />
+        <Drawer.Screen name="Directory" component={DirectoryNavigator} options={{ title: "Directory" }} />
+      </Drawer.Navigator>
     </View>
   );
 };
