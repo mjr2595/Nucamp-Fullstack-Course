fc48b1b509e9a5080f0f008ae30e160caacf05f0
mjr2595 <michaeljohnraymond@gmail.com>
2023-03-07 21:18:01 -0600

    Sending Email

--- NucampFolder/4-React-Native/nucampsite/screens/ContactScreen.js
+++ NucampFolder/4-React-Native/nucampsite/screens/ContactScreen.js
@@ -1,8 +1,16 @@
 import * as Animatable from "react-native-animatable";
-import { Card, Text } from "react-native-elements";
+import { Card, Text, Button, Icon } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
+import * as MailComposer from "expo-mail-composer";
 
 const ContactScreen = () => {
+  const sendMail = () => {
+    MailComposer.composeAsync({
+      recipients: ["campsites@nucamp.co"],
+      subject: "Inquiry",
+      body: "To whom it may concern:",
+    });
+  };
   return (
     <ScrollView>
       <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
@@ -14,6 +22,12 @@ const ContactScreen = () => {
           <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
           <Text>Phone: 1-206-555-1234</Text>
           <Text>Email: campsites@nucamp.co</Text>
+          <Button
+            title="Send Email"
+            buttonStyle={{ backgroundColor: "#5637DD", margin: 40 }}
+            icon={<Icon name="envelope-o" type="font-awesome" color="#fff" iconStyle={{ marginRight: 10 }} />}
+            onPress={() => sendMail()}
+          />
         </Card>
       </Animatable.View>
     </ScrollView>
