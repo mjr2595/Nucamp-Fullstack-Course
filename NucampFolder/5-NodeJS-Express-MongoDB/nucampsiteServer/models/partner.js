2d663fc3b524851abecb4749c82828311f2d5e01
mjr2595 <michaeljohnraymond@gmail.com>
2023-04-01 11:43:18 -0500

    Week 2 Workshop Assignment

---
+++ NucampFolder/5-NodeJS-Express-MongoDB/nucampsiteServer/models/partner.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const partnerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Partner", partnerSchema);
